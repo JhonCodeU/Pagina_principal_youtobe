c0ce3b495b40ce421c8ec5bd06673393615a3f7d
JhonCodeU <jhon.380172793@ucaldas.edu.co>
2020-01-26 16:03:00 -0500

    version 3

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ $(document).ready(function () {
         $('#contenedor').toggleClass('active');//quitar y poner la clase naranja
     });
 });
+
+const comprobarAncho = function(){
+	if(window.innerWidth <= 768){
+		$('#contenedor').removeClass('active');
+	}else{
+		$('#contenedor').addClass('active');
+	}
+};
+comprobarAncho();
+window.addEventListener('resize', function(){
+	comprobarAncho();
+});
